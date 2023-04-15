3d6b35f1cf55668b273e5dbce3cdfc9e879cdb54
Narmin Molina <narminmolina@gmail.com>
2023-04-15 05:56:55 -0400

    close btn

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { LikeIcon } from '@/svg/LikeIcon';
 import { convertBytesToMB, classNames } from '@/utils';
 import { useDispatch } from '@/redux/store';
-import { selectActiveImage, setDeleteImage, setImageAsFavorited } from '@/redux/reducer';
+import { selectActiveImage, setActiveTab, setAsideProps, setDeleteImage, setImageAsFavorited } from '@/redux/reducer';
 import styles from '@/styles/modules/Aside.module.css';
 import { formatDate } from '@/utils';
 
@@ -15,6 +15,10 @@ export const Aside = ({ isOpen }) => {
 
   const handleDeleteButtonClick = useCallback(() => {
     dispatch(setDeleteImage(id));
+  }, []);
+
+  const handleCloseButtonClick = useCallback(() => {
+    dispatch(setAsideProps({ isAsideOpen: false, activeImage: null }));
   }, []);
   const handleFavoriteButtonClick = useCallback(() => {
     dispatch(setImageAsFavorited(id));
@@ -40,6 +44,21 @@ export const Aside = ({ isOpen }) => {
 
   return (
     <aside className={classNames(styles.aside, isOpen ? styles.visible : '')}>
+      <button className={styles.closeButton} onClick={handleCloseButtonClick}>
+        <svg
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#a7a7a7"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <line x1="18" y1="6" x2="6" y2="18"></line>
+          <line x1="6" y1="6" x2="18" y2="18"></line>
+        </svg>
+      </button>
       <img src={url} alt={filename} />
       <header>
         <div className={styles.imageHeader}>
